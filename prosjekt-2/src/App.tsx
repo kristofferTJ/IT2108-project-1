007ec5d39b0743313df53623b1cf833cb3d76b0e
Kristoffer Tufte-Johnsen <kristotu@stud.ntnu.no>
2020-09-14 17:23:20 +0200

    Begynnesle på en meny #9

--- prosjekt-2/src/App.tsx
+++ prosjekt-2/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './App.css';
+import Sidebar from './components/Menu/Menu';
+import { GlobalStateContext } from './context/GlobalStateContext';
 
 function App() {
+  const { imgProvider, soundProvider, poemProvider } = useContext(
+    GlobalStateContext
+  )!;
+
   return (
     <div className="App">
-      
+      <Sidebar></Sidebar>
+      <p>bilde: {imgProvider.activeImg}</p>
+      <p>lyd: {soundProvider.activeSound}</p>
+      <p>dikt: {poemProvider.activePoem}</p>
     </div>
   );
 }
--- prosjekt-2/src/index.tsx
+++ prosjekt-2/src/index.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import GlobalStateProvider from './context/GlobalStateContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStateProvider>
+      <App />
+    </GlobalStateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
