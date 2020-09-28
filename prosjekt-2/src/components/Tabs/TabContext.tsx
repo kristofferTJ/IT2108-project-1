a9821ea00b3fb7f49d698edb9467595df3a6d37d
Benedicte <benedkh@stud.ntnu.no>
2020-09-28 14:34:47 +0200

    Fjernet logo.svg og tabContext som ikke brukes lengre

--- prosjekt-2/src/components/Tabs/TabContext.tsx
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-interface TabInterface {
-  activeTab: number;
-  setActiveTab: (value: number) => void;
-}
-
-export const tabsContext = createContext<TabInterface | undefined>(undefined);
